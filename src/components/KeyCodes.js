e654f4f7607f05d04746815cf2378d7b0e62ba86
Razvan <razvan.lada@gmail.com>
2017-11-12 22:28:47 -0500

    Added missing key binding.

--- src/components/KeyCodes.js
+++ src/components/KeyCodes.js
@@ -12,14 +12,48 @@ const digitKeyCodes = {
 };
 
 const operatorKeyCodes = {
-  48: [{ key: ')', shiftKeyPressed: true }],
-  56: [{ key: '*', shiftKeyPressed: true }],
-  57: [{ key: '(', shiftKeyPressed: true }],
-  187: [{ key: '+', shiftKeyPressed: true}, { key: '=', shiftKeyPressed: false}],
-  109: [{ key: '-', shiftKeyPressed: true}],
-  111: [{ key: '/', shiftKeyPressed: false}],
-  191: [{ key: '/', shiftKeyPressed: false}],
-  13: [{ key: '=', shiftKeyPressed: false}],
+  13: [{
+    key: '=',
+    shiftKeyPressed: false
+  }],
+  48: [{
+    key: ')',
+    shiftKeyPressed: true
+  }],
+  56: [{
+    key: '*',
+    shiftKeyPressed: true
+  }],
+  57: [{
+    key: '(',
+    shiftKeyPressed: true
+  }],
+  187: [{
+    key: '+',
+    shiftKeyPressed: true
+  }, {
+    key: '=',
+    shiftKeyPressed: false
+  }],
+  109: [{
+    key: '-',
+    shiftKeyPressed: true
+  }],
+  111: [{
+    key: '/',
+    shiftKeyPressed: false
+  }],
+  190: [{
+    key: '.',
+    shiftKeyPressed: false
+  }],
+  191: [{
+    key: '/',
+    shiftKeyPressed: false
+  }]
 };
 
-export { digitKeyCodes, operatorKeyCodes }
+export {
+  digitKeyCodes,
+  operatorKeyCodes
+}
